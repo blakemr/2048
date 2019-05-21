71b56eab010ab3fa1c5ebb602035fc00f642195e
Matthew Blake <mblak0625@gmail.com>
2019-05-21 19:11:36 -0400

    added simpleState to hold main game state

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,23 @@ use amethyst::{
     utils::application_root_dir,
 };
 
-struct Example;
-
-impl SimpleState for Example {}
+mod two_eleven;
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
-    let path = format!(
-        "{}/resources/display_config.ron",
-        application_root_dir()
-    );
+    let path = format!("{}/resources/display_config.ron", application_root_dir());
     let config = DisplayConfig::load(&path);
 
     let pipe = Pipeline::build().with_stage(
         Stage::with_backbuffer()
-            .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
+            .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
             .with_pass(DrawFlat::<PosNormTex>::new()),
     );
 
     let game_data =
         GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
-    let mut game = Application::new("./", Example, game_data)?;
+    let mut game = Application::new("./", two_eleven::TwoEleven, game_data)?;
 
     game.run();
 
---
+++ src/two_eleven.rs
@@ -0,0 +1,8 @@
+/// 2048 main state
+use amethyst::prelude::*;
+
+pub struct TwoEleven;
+
+impl SimpleState for TwoEleven {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {}
+}
